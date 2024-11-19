025f6afb92a9535ee85931645ea1a6441676e65d
Alejandro Martínez <amj.parrales@gmail.com>
2024-11-19 11:18:15 +0100

    Primer commit con la UI de la aplicacion.

---
+++ app/src/main/java/com/example/pmdm_tarea_2/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.pmdm_tarea_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webView);
+
+        webView.loadUrl("https://www.google.com");
+    }
+}
